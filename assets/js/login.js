f558c42e9b4e27b1d210132ec4aac10ae4149d45
chenpeng <chenpeng9201@hotmail.com>
2022-06-18 21:48:03 +0800

    13注册表单新增确认密码校验规则

--- assets/js/login.js
+++ assets/js/login.js
@@ -18,6 +18,13 @@ $(function() {
     form.verify({
         pwd: [
             /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
-        ]
+        ],
+
+        repwd: function(value) {
+            var pwd = $('.reg-box [name=password]').val();
+            if (pwd !== value) {
+                return "两次密码输入不一致!"
+            }
+        }
     });
 });
