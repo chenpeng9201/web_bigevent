f2843794023a38c6089b1eff381f09ad7c7681a1
chenpeng <472126367@qq.com>
2022-06-21 15:18:10 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,22 @@
 // 给ajax设置请求根路径
 $.ajaxPrefilter(function(options) {
     options.url = 'http://www.liulongbin.top:3007' + options.url;
+    // options.url = 'http://big-event-api-t.itheima.net' + options.url;
+    // 为需要权限验证的接口统一添加请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || "",
+        }
+    }
+
+    // 为全局挂在complete回调函数
+    options.complete = function(res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            // 认证失败，跳转到登录页面
+            // 回到登录页
+            location.href = '/login.html';
+            // 清空token
+            localStorage.removeItem('token');
+        }
+    }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,10 @@
 $(function() {
+    var layer = layui.layer;
     getUserInfo();
+    // 监听退出按钮点击事件
+    $('#logout').on('click', function() {
+        logout();
+    })
 });
 
 // 获取用户信息
@@ -7,12 +12,55 @@ function getUserInfo() {
     $.ajax({
         method: "GET",
         url: "/my/userinfo",
-        Headers: {
-            Authorization: localStorage.getItem('token') || ''
-        },
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || "",
+        // },
         success: function(res) {
-            console.log(localStorage.getItem('token'));
-            console.log(res);
-        }
+                if (res.status !== 0) {
+                    return layer.msg("获取用户信息失败！");
+                }
+                renderAvatar(res.data);
+            }
+            // complete: function(res) {
+            //     if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            //         // 认证失败，跳转到登录页面
+            //         // 回到登录页
+            //         location.href = '/login.html';
+            //         // 清空token
+            //         localStorage.removeItem('token');
+            //     }
+            // }
+    });
+}
+
+//用户头像渲染
+function renderAvatar(user) {
+    var name = user.nickname || user.username || '';
+    // 设置用户姓名
+    $("#welcome").html('欢迎&nbsp;&nbsp;' + name);
+
+    if (user.user_pic !== null) {
+        //优先渲染图片图像
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        // 隐藏文本头像
+        $('.text-avatar').hide();
+    } else {
+        // 其次渲染文本头像
+        $('.layui-nav-img').hide();
+        var firstnm = name[0].toUpperCase();
+        $('.text-avatar').html(firstnm).show();
+
+    }
+}
+
+// 退出登录
+function logout() {
+    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+        // 回到登录页
+        location.href = '/login.html';
+        // 清空token
+        localStorage.removeItem('token');
+        //关闭确认框
+        layer.close(index);
     });
 }
